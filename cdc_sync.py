0976a46a8a5aa8c0da742c74723104c82a1e13f2
Neslihan Özdemir <neslihanozdmr.4871@gmail.com>
2026-04-28 20:56:25 +0300

    Update cdc_sync.py

--- cdc_sync.py
+++ cdc_sync.py
@@ -5,7 +5,7 @@ from pymongo import MongoClient
 mysql_conn = mysql.connector.connect(
     host="localhost",
     user="root",
-    password="şifrenizi_buraya_yazın",
+    password="şifre",
     database="cdc_db",
     autocommit=True
 )
